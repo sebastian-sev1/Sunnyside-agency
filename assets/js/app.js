ccfaa5d1b26f8a4ca36e18c4d016ec49e58d410c
Sebastian <sebastianseverianosv@gmail.com>
2022-12-20 13:52:09 -0600

    se agrego mas contenido al sitio web

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,11 @@ const iconMenu = document.getElementById("icon-menu");
 let menu = document.querySelector(".header-menu");
 
 function menuToggle(){
-    menu.classList.toggle("active")
+    let links = document.querySelectorAll(".link-item");
+    links.forEach((link)=>{
+        link.addEventListener("click",menuToggle);
+    });
+    menu.classList.toggle("active");
 }
 
 iconMenu.addEventListener("click",menuToggle);
